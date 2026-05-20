3e6823dfd52aff159c01bc9b30a9b9a7029ca258
Maicol Perez <stevenperez9102@gmail.com>
2026-05-20 13:42:44 -0500

    fix neon database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "aurum_piano_db",
-  password: process.env.DB_PASSWORD || "1234",
-  port: Number(process.env.DB_PORT) || 5432,
-});
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: process.env.DB_USER || "postgres",
+        host: process.env.DB_HOST || "localhost",
+        database: process.env.DB_NAME || "aurum_piano_db",
+        password: process.env.DB_PASSWORD || "1234",
+        port: Number(process.env.DB_PORT) || 5432,
+      }
+);
 
 module.exports = pool;
